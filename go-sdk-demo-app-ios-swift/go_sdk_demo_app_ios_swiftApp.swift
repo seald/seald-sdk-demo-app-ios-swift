ddbcb31ef8bbac98d36780f6ef50039a37541029
mehdi <mehdi@seald.io>
2023-02-28 11:09:56 +0100

    Initial Commit

---
+++ go-sdk-demo-app-ios-swift/go_sdk_demo_app_ios_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  go_sdk_demo_app_ios_swiftApp.swift
+//  go-sdk-demo-app-ios-swift
+//
+//  Created by Mehdi Kouhen on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct go_sdk_demo_app_ios_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
